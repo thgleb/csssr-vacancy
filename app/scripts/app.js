1a4439bf43c5756fd734995a34f9c933b1123ed3
Gleb Tsyrulnikov <glebserferus@gmail.com>
2017-07-16 17:01:01 +0300

    removed jquery from app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,5 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-
 import AutoResizeTextarea from './textarea-autoresize.js';
 import Range from './range.js';
-
-$(() => {
-	svg4everybody();
-});
 
 const textarea = new AutoResizeTextarea(document.querySelector('.textarea'));
 textarea.initialize();

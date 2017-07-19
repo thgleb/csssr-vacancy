755d79579ba1e59b4ccd1996163998d3f7a05df9
Gleb Tsyrulnikov <glebserferus@gmail.com>
2017-07-20 02:06:56 +0300

    changed Range from ES5-style into ES2015-style (classes)

--- app/scripts/range.js
+++ app/scripts/range.js
@@ -1,53 +1,50 @@
-function Range(elem) {
-	this._elem = elem;
+class Range {
+	constructor(elem) {
+		this._elem = elem;
 
-	if (!this._elem) {
-		return;
+		if (!this._elem) {
+			return;
+		}
+
+		this._control = this._elem.querySelector('.range__control');
+		this._options = this._elem.querySelectorAll('.range__visual-option');
+
+		if (!this._control || this._options.length === 0) {
+			return;
+		}
+
+		this._clickListeners = [];
 	}
+	initialize() {
+		[...this._options].forEach(option => {
+			const listener = this._clickHandler.bind(this);
 
-	this._control = this._elem.querySelector('.range__control');
-	this._options = this._elem.querySelectorAll('.range__visual-option');
-
-	if (!this._control || this._options.length === 0) {
-		return;
+			option.addEventListener('click', listener);
+			this._clickListeners.push(listener);
+		});
 	}
+	destroy() {
+		[...this._options].forEach((option, i) => {
+			option.removeEventListener('click', this._clickListeners[i]);
+		});
 
-	this._clickListeners = [];
+		this._clickListeners = [];
+	}
+	_clickHandler(e) {
+		e.preventDefault();
+
+		const value = parseInt(e.currentTarget.getAttribute('data-value'), 10) || 0;
+
+		this._clearCheckedOptions();
+		this._setCheckedOption(value);
+	}
+	_clearCheckedOptions() {
+		[...this._options].forEach(option => option.removeAttribute('data-checked'));
+	}
+	_setCheckedOption(index) {
+		this._control.value = index;
+		this._options[index].setAttribute('data-checked', true);
+	}
 }
-
-Range.prototype.initialize = function () {
-	[...this._options].forEach(option => {
-		const listener = this._clickHandler.bind(this);
-
-		option.addEventListener('click', listener);
-		this._clickListeners.push(listener);
-	});
-};
-
-Range.prototype.destroy = function () {
-	[...this._options].forEach((option, i) => {
-		option.removeEventListener('click', this._clickListeners[i]);
-	});
-
-	this._clickListeners = [];
-};
-
-Range.prototype._clickHandler = function (e) {
-	e.preventDefault();
-
-	const value = parseInt(e.currentTarget.getAttribute('data-value'), 10) || 0;
-
-	this._clearCheckedOptions();
-	this._setCheckedOption(value);
-};
-
-Range.prototype._clearCheckedOptions = function () {
-	[...this._options].forEach(option => option.removeAttribute('data-checked'));
-};
-
-Range.prototype._setCheckedOption = function (index) {
-	this._control.value = index;
-	this._options[index].setAttribute('data-checked', true);
-};
 
 export default Range;
